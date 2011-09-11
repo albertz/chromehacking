a240af20925df524e781f3d9d85b87b56856787b
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-09-11 23:02:33 +0200

    more hacking

--- pyobjcdemo.py
+++ pyobjcdemo.py
@@ -1,81 +1,87 @@
+import os
+import sys
 
-from __future__ import division
-"""
- backend_cocoaagg.py
-
- A native Cocoa backend via PyObjC in OSX.
-
- Author: Charles Moad (cmoad@users.sourceforge.net)
-
- Notes:
-  - Requires PyObjC (currently testing v1.3.7)
-  - The Tk backend works nicely on OSX.  This code
-    primarily serves as an example of embedding a
-    matplotlib rendering context into a cocoa app
-    using a NSImageView.
-"""
-
-import os, sys
-
-try:
-    import objc
-except:
-    print >>sys.stderr, 'The CococaAgg backend required PyObjC to be installed!'
-    print >>sys.stderr, '  (currently testing v1.3.7)'
-    sys.exit()
-
+# SDL-ctypes on OS X requires PyObjC
 from Foundation import *
 from AppKit import *
-from PyObjCTools import NibClassBuilder, AppHelper
+import objc
+import MacOS
 
+# Need to do this if not running with a nib
+def setupAppleMenu(app):
+    return
+    appleMenuController = NSAppleMenuController.alloc().init()
+    appleMenuController.retain()
+    appleMenu = NSMenu.alloc().initWithTitle_('')
+    appleMenuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_('', None, '')
+    appleMenuItem.setSubmenu_(appleMenu)
+    app.mainMenu().addItem_(appleMenuItem)
+    appleMenuController.controlMenu_(appleMenu)
+    app.mainMenu().removeItem_(appleMenuItem)
 
-mplBundle = NSBundle.bundleWithPath_(os.path.dirname(__file__))
+# Need to do this if not running with a nib
+def setupWindowMenu(app):
+    windowMenu = NSMenu.alloc().initWithTitle_('Window')
+    windowMenu.retain()
+    menuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_('Minimize', 'performMiniaturize:', 'm')
+    windowMenu.addItem_(menuItem)
+    windowMenuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_('Window', None, '')
+    windowMenuItem.setSubmenu_(windowMenu)
+    app.mainMenu().addItem_(windowMenuItem)
+    app.setWindowsMenu_(windowMenu)
 
+# Used to cleanly terminate
+class SDLAppDelegate(NSObject):
+    def applicationShouldTerminate_(self, app):
+        event = SDL.events.SDL_Event()
+        event.type = SDL_QUIT
+        SDL.events.SDL_PushEvent(event)
+        return NSTerminateLater
 
+    def windowUpdateNotification_(self, notification):
+        win = notification.object()
+        if not SDL.dll.version_compatible((1, 2, 8)) and isinstance(win, objc.lookUpClass('SDL_QuartzWindow')):
+            # Seems to be a retain count bug in SDL.. workaround!
+            win.retain()
+        NSNotificationCenter.defaultCenter().removeObserver_name_object_(
+            self, NSWindowDidUpdateNotification, None)
+        self.release()
 
+def setIcon(app, icon_data):
+    data = NSData.dataWithBytes_length_(icon_data, len(icon_data))
+    if data is None:
+        return
+    img = NSImage.alloc().initWithData_(data)
+    if img is None:
+        return
+    app.setApplicationIconImage_(img)
 
-class MPLBootstrap(NSObject):
-    # Loads the nib containing the PlotWindow and PlotView
-    def startWithBundle_(self, bundle):
-        #NSApplicationLoad()
-        if not bundle.loadNibFile_externalNameTable_withZone_('Matplotlib.nib', {}, None):
-            print >>sys.stderr, 'Unable to load Matplotlib Cocoa UI!'
-            sys.exit()
+def install():
+    app = NSApplication.sharedApplication()
+    appDelegate = SDLAppDelegate.alloc().init()
+    app.setDelegate_(appDelegate)
+    appDelegate.retain()
+    NSNotificationCenter.defaultCenter().addObserver_selector_name_object_(
+        appDelegate,
+        'windowUpdateNotification:',
+        NSWindowDidUpdateNotification,
+        None)
+    if not app.mainMenu():
+        mainMenu = NSMenu.alloc().init()
+        app.setMainMenu_(mainMenu)
+        setupAppleMenu(app)
+        setupWindowMenu(app)
+    app.finishLaunching()
+    app.updateWindows()
+    app.activateIgnoringOtherApps_(True)
 
-class FigureManagerCocoaAgg:
-    def __init__(self):
-        #FigureManagerBase.__init__(self, canvas, num)
-
-        try:
-            WMEnable('Matplotlib')
-        except:
-            # MULTIPLE FIGURES ARE BUGGY!
-            pass # If there are multiple figures we only need to enable once
-        #self.bootstrap = MPLBootstrap.alloc().init().performSelectorOnMainThread_withObject_waitUntilDone_(
-        #        'startWithBundle:',
-        #        mplBundle,
-        #        False)
-
-    def show(self):
-        # Load a new PlotWindow
-        self.bootstrap = MPLBootstrap.alloc().init().performSelectorOnMainThread_withObject_waitUntilDone_(
-                'startWithBundle:',
-                mplBundle,
-                False)
-        NSApplication.sharedApplication().run()
-
-
-FigureManager = FigureManagerCocoaAgg
-
-#### Everything below taken from PyObjC examples
-#### This is a hack to allow python scripts to access
-#### the window manager without running pythonw.
 def S(*args):
     return ''.join(args)
 
 OSErr = objc._C_SHT
 OUTPSN = 'o^{ProcessSerialNumber=LL}'
 INPSN = 'n^{ProcessSerialNumber=LL}'
+
 FUNCTIONS=[
     # These two are public API
     ( u'GetCurrentProcess', S(OSErr, OUTPSN) ),
@@ -84,18 +90,20 @@ FUNCTIONS=[
     ( u'CPSSetProcessName', S(OSErr, INPSN, objc._C_CHARPTR) ),
     ( u'CPSEnableForegroundOperation', S(OSErr, INPSN) ),
 ]
-def WMEnable(name='Python'):
+
+def WMEnable(name=None):
+    if name is None:
+        name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
     if isinstance(name, unicode):
-        name = name.encode('utf8')
-    mainBundle = NSBundle.mainBundle()
-    bPath = os.path.split(os.path.split(os.path.split(sys.executable)[0])[0])[0]
-    if mainBundle.bundlePath() == bPath:
-        return True
+        name = name.encode('utf-8')
+    if not hasattr(objc, 'loadBundleFunctions'):
+        return False
     bndl = NSBundle.bundleWithPath_(objc.pathForFramework('/System/Library/Frameworks/ApplicationServices.framework'))
     if bndl is None:
         print >>sys.stderr, 'ApplicationServices missing'
         return False
     d = {}
+    app = NSApplication.sharedApplication()
     objc.loadBundleFunctions(bndl, d, FUNCTIONS)
     for (fn, sig) in FUNCTIONS:
         if fn not in d:
@@ -111,7 +119,7 @@ def WMEnable(name='Python'):
         return False
     err = d['CPSEnableForegroundOperation'](psn)
     if err:
-        #print >>sys.stderr, 'CPSEnableForegroundOperation', (err, psn)
+        print >>sys.stderr, 'CPSEnableForegroundOperation', (err, psn)
         return False
     err = d['SetFrontProcess'](psn)
     if err:
@@ -120,8 +128,20 @@ def WMEnable(name='Python'):
     return True
 
 
+def init():
+    if not (MacOS.WMAvailable() or WMEnable()):
+        raise ImportError, "Can not access the window manager.  Use py2app or execute with the pythonw script."
+    if not NSApp():
+        # running outside of a bundle
+        install()
+    # running inside a bundle, change dir
+    if (os.getcwd() == '/') and len(sys.argv) > 1:
+        os.chdir(os.path.dirname(sys.argv[0]))
+    return True
 
-WMEnable()
+init()
+print "after init"
+
 NSApplication.sharedApplication().run()
 
 
