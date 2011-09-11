4f83d387547757c49d345d5d4f1af4e66769df30
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-09-11 22:59:00 +0200

    more hacking

--- pyobjcdemo.py
+++ pyobjcdemo.py
@@ -1,9 +1,142 @@
+
+from __future__ import division
+"""
+ backend_cocoaagg.py
+
+ A native Cocoa backend via PyObjC in OSX.
+
+ Author: Charles Moad (cmoad@users.sourceforge.net)
+
+ Notes:
+  - Requires PyObjC (currently testing v1.3.7)
+  - The Tk backend works nicely on OSX.  This code
+    primarily serves as an example of embedding a
+    matplotlib rendering context into a cocoa app
+    using a NSImageView.
+"""
+
 import os, sys
+
+try:
+    import objc
+except:
+    print >>sys.stderr, 'The CococaAgg backend required PyObjC to be installed!'
+    print >>sys.stderr, '  (currently testing v1.3.7)'
+    sys.exit()
+
+from Foundation import *
+from AppKit import *
+from PyObjCTools import NibClassBuilder, AppHelper
+
+
+mplBundle = NSBundle.bundleWithPath_(os.path.dirname(__file__))
+
+
+
+
+class MPLBootstrap(NSObject):
+    # Loads the nib containing the PlotWindow and PlotView
+    def startWithBundle_(self, bundle):
+        #NSApplicationLoad()
+        if not bundle.loadNibFile_externalNameTable_withZone_('Matplotlib.nib', {}, None):
+            print >>sys.stderr, 'Unable to load Matplotlib Cocoa UI!'
+            sys.exit()
+
+class FigureManagerCocoaAgg:
+    def __init__(self):
+        #FigureManagerBase.__init__(self, canvas, num)
+
+        try:
+            WMEnable('Matplotlib')
+        except:
+            # MULTIPLE FIGURES ARE BUGGY!
+            pass # If there are multiple figures we only need to enable once
+        #self.bootstrap = MPLBootstrap.alloc().init().performSelectorOnMainThread_withObject_waitUntilDone_(
+        #        'startWithBundle:',
+        #        mplBundle,
+        #        False)
+
+    def show(self):
+        # Load a new PlotWindow
+        self.bootstrap = MPLBootstrap.alloc().init().performSelectorOnMainThread_withObject_waitUntilDone_(
+                'startWithBundle:',
+                mplBundle,
+                False)
+        NSApplication.sharedApplication().run()
+
+
+FigureManager = FigureManagerCocoaAgg
+
+#### Everything below taken from PyObjC examples
+#### This is a hack to allow python scripts to access
+#### the window manager without running pythonw.
+def S(*args):
+    return ''.join(args)
+
+OSErr = objc._C_SHT
+OUTPSN = 'o^{ProcessSerialNumber=LL}'
+INPSN = 'n^{ProcessSerialNumber=LL}'
+FUNCTIONS=[
+    # These two are public API
+    ( u'GetCurrentProcess', S(OSErr, OUTPSN) ),
+    ( u'SetFrontProcess', S(OSErr, INPSN) ),
+    # This is undocumented SPI
+    ( u'CPSSetProcessName', S(OSErr, INPSN, objc._C_CHARPTR) ),
+    ( u'CPSEnableForegroundOperation', S(OSErr, INPSN) ),
+]
+def WMEnable(name='Python'):
+    if isinstance(name, unicode):
+        name = name.encode('utf8')
+    mainBundle = NSBundle.mainBundle()
+    bPath = os.path.split(os.path.split(os.path.split(sys.executable)[0])[0])[0]
+    if mainBundle.bundlePath() == bPath:
+        return True
+    bndl = NSBundle.bundleWithPath_(objc.pathForFramework('/System/Library/Frameworks/ApplicationServices.framework'))
+    if bndl is None:
+        print >>sys.stderr, 'ApplicationServices missing'
+        return False
+    d = {}
+    objc.loadBundleFunctions(bndl, d, FUNCTIONS)
+    for (fn, sig) in FUNCTIONS:
+        if fn not in d:
+            print >>sys.stderr, 'Missing', fn
+            return False
+    err, psn = d['GetCurrentProcess']()
+    if err:
+        print >>sys.stderr, 'GetCurrentProcess', (err, psn)
+        return False
+    err = d['CPSSetProcessName'](psn, name)
+    if err:
+        print >>sys.stderr, 'CPSSetProcessName', (err, psn)
+        return False
+    err = d['CPSEnableForegroundOperation'](psn)
+    if err:
+        #print >>sys.stderr, 'CPSEnableForegroundOperation', (err, psn)
+        return False
+    err = d['SetFrontProcess'](psn)
+    if err:
+        print >>sys.stderr, 'SetFrontProcess', (err, psn)
+        return False
+    return True
+
+
+
+WMEnable()
+NSApplication.sharedApplication().run()
+
+
+print "XXXXXX"
+
+import os, sys
+sys.exit()
+
 
 import objc
 from Foundation import *
 from AppKit import *
 from PyObjCTools import AppHelper
+
+#pool = NSAutoreleasePool.alloc().init()
 
 class MyAppDelegate(NSObject):
 
