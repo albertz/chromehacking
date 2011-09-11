a507ed4ff352cc69f78c43b2e2bc2131fe86d8ae
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-09-11 23:21:52 +0200

    cleanup

--- pyobjcdemo.py
+++ pyobjcdemo.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import os
 import sys
 
@@ -15,13 +17,6 @@ def setupWindowMenu(app):
     app.mainMenu().addItem_(windowMenuItem)
     app.setWindowsMenu_(windowMenu)
 
-class MyAppDelegate(NSObject):
-    def windowUpdateNotification_(self, notification):
-        win = notification.object()
-        NSNotificationCenter.defaultCenter().removeObserver_name_object_(
-            self, NSWindowDidUpdateNotification, None)
-        self.release()
-
 def setIcon(app, icon_data):
     data = NSData.dataWithBytes_length_(icon_data, len(icon_data))
     if data is None:
@@ -31,82 +26,14 @@ def setIcon(app, icon_data):
         return
     app.setApplicationIconImage_(img)
 
-def install():
-    app = NSApplication.sharedApplication()
-    appDelegate = MyAppDelegate.alloc().init()
-    app.setDelegate_(appDelegate)
-    appDelegate.retain()
-    NSNotificationCenter.defaultCenter().addObserver_selector_name_object_(
-        appDelegate,
-        'windowUpdateNotification:',
-        NSWindowDidUpdateNotification,
-        None)
-    if not app.mainMenu():
-        mainMenu = NSMenu.alloc().init()
-        app.setMainMenu_(mainMenu)
-        setupWindowMenu(app)
-    app.finishLaunching()
-    app.updateWindows()
-    app.activateIgnoringOtherApps_(True)
+app = NSApplication.sharedApplication()
 
-def S(*args):
-    return ''.join(args)
+mainMenu = NSMenu.alloc().init()
+app.setMainMenu_(mainMenu)
+setupWindowMenu(app)
 
-OSErr = objc._C_SHT
-OUTPSN = 'o^{ProcessSerialNumber=LL}'
-INPSN = 'n^{ProcessSerialNumber=LL}'
+app.finishLaunching()
+app.updateWindows()
+app.activateIgnoringOtherApps_(True)
 
-FUNCTIONS=[
-    # These two are public API
-    ( u'GetCurrentProcess', S(OSErr, OUTPSN) ),
-    ( u'SetFrontProcess', S(OSErr, INPSN) ),
-    # This is undocumented SPI
-    ( u'CPSSetProcessName', S(OSErr, INPSN, objc._C_CHARPTR) ),
-    ( u'CPSEnableForegroundOperation', S(OSErr, INPSN) ),
-]
-
-def WMEnable(name=None):
-    if name is None:
-        name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
-    if isinstance(name, unicode):
-        name = name.encode('utf-8')
-    if not hasattr(objc, 'loadBundleFunctions'):
-        return False
-    bndl = NSBundle.bundleWithPath_(objc.pathForFramework('/System/Library/Frameworks/ApplicationServices.framework'))
-    if bndl is None:
-        print >>sys.stderr, 'ApplicationServices missing'
-        return False
-    d = {}
-    app = NSApplication.sharedApplication()
-    objc.loadBundleFunctions(bndl, d, FUNCTIONS)
-    for (fn, sig) in FUNCTIONS:
-        if fn not in d:
-            print >>sys.stderr, 'Missing', fn
-            return False
-    err, psn = d['GetCurrentProcess']()
-    if err:
-        print >>sys.stderr, 'GetCurrentProcess', (err, psn)
-        return False
-    err = d['CPSSetProcessName'](psn, name)
-    if err:
-        print >>sys.stderr, 'CPSSetProcessName', (err, psn)
-        return False
-    err = d['CPSEnableForegroundOperation'](psn)
-    if err:
-        print >>sys.stderr, 'CPSEnableForegroundOperation', (err, psn)
-        return False
-    err = d['SetFrontProcess'](psn)
-    if err:
-        print >>sys.stderr, 'SetFrontProcess', (err, psn)
-        return False
-    return True
-
-
-def init():
-    #if not WMEnable():
-    #    raise ImportError, "Can not access the window manager.  Use py2app or execute with the pythonw script."
-    install()
-
-install()
-
-NSApplication.sharedApplication().run()
+app.run()
