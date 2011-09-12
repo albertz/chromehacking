825ac3d1405bbefebbd9c2ec3ca85371d2adb5c7
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-09-12 02:36:42 +0200

    more. mostly finished

--- pyobjcdemo.py
+++ pyobjcdemo.py
@@ -3,41 +3,39 @@
 # http://codesearch.google.com/#u_9_nDrchrw/pygame-1.7.1release/lib/macosx.py&ct=rc&cd=6&q=GetCurrentProcess%20ApplicationServices%20%20file:.py
 # http://codesearch.google.com/#TjZxI4W0_Cw/trunk/cocos/audio/SDL/darwin.py&ct=rc&cd=2&q=GetCurrentProcess%20ApplicationServices%20%20file:.py
 
-import os
-import sys
+import os, sys
+from pprint import pprint
 
 from Foundation import *
 from AppKit import *
 import objc
 
-def setIcon(app, icon_data):
-    data = NSData.dataWithBytes_length_(icon_data, len(icon_data))
-    if data is None:
-        return
-    img = NSImage.alloc().initWithData_(data)
-    if img is None:
-        return
-    app.setApplicationIconImage_(img)
-
-app = NSApplication.sharedApplication()
+def setIcon(baseurl):
+	url = NSURL.URLWithString_relativeToURL_("/favicon.ico", NSURL.URLWithString_(baseurl))
+	img = NSImage.alloc().initWithContentsOfURL_(url)
+	if img is None:
+		return
+	app.setApplicationIconImage_(img)
 
 class MyAppDelegate(NSObject):
 	def applicationShouldHandleReopen_hasVisibleWindows_(self, app, flag):
 		print "click"
 
 	def applicationDidFinishLaunching_(self, notification):
-		
 		menu = NSMenu.alloc().init()
 		menu.addItem_(NSMenuItem.alloc().initWithTitle_action_keyEquivalent_('View Trash', 'view:', ''))
 		menu.addItem_(NSMenuItem.alloc().initWithTitle_action_keyEquivalent_('Empty Trash', 'empty:', ''))
 		menu.addItem_(NSMenuItem.separatorItem())
 		menu.addItem_(NSMenuItem.alloc().initWithTitle_action_keyEquivalent_('Quit', 'terminate:', 'q'))
 		
-		windowMenuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_('Window', None, '')
+		windowMenuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_('Foobar', None, '')
 		windowMenuItem.setSubmenu_(menu)
 		
 		app.setMainMenu_(NSMenu.alloc().init())
 		app.mainMenu().addItem_(windowMenuItem)
+		
+		try: setIcon(sys.argv[1])
+		except: pass
 		
 	def open_window(self):
 		self.window = NSWindow.alloc().initWithContentRect_styleMask_backing_defer_(
@@ -46,7 +44,8 @@ class MyAppDelegate(NSObject):
 			NSBackingStoreBuffered,
 			objc.NO)
 		self.window.setIsVisible_(True)
-		
+
+app = NSApplication.sharedApplication()
 delegate = MyAppDelegate.alloc().init()
 app.setDelegate_(delegate)
 app.finishLaunching()
