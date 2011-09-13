958a4a6d9d619e01ac872522ac228f0952f46982
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-09-13 17:19:35 +0200

    even nicer example

--- applescript_client.py
+++ applescript_client.py
@@ -9,3 +9,18 @@ def execPy(cmd):
 	return app.event("CrSuExPy", {"comm": cmd}).send()
 
 print execPy("app.bundle()")
+
+def simple_shell():
+	import sys
+	try: import readline
+	except: pass # ignore
+	while True:
+		try:
+			s = raw_input("> ")
+		except:
+			print "breaked debug shell:", sys.exc_info()[0].__name__
+			break
+		s = s.strip()
+		if s: print execPy(s)
+
+simple_shell()
